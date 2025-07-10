ee921674c65ed6af133a730398b37f614f12eec9
akshat k <inquiry.akshatkotpalliwar@gmail.com>
2025-07-10 20:04:37 +0530

    fix tab buttons

--- src/components/TabsContainer.tsx
+++ src/components/TabsContainer.tsx
@@ -36,42 +36,6 @@ const TabsContainer: React.FC<TabsContainerProps> = ({ theme, needUpdate }) => {
           backgroundColor: `var(--ion-color-background-${theme})`,
         }}
       >
-        <IonToolbar style={{ '--background': `var(--ion-color-transparent-${theme})` }}>
-          <IonTabBar
-            className={theme}
-            slot="top"
-            color={`transparent-${theme}`}
-          >
-            <IonTabButton
-              tab="menu"
-              href="#"
-            >
-              <IonMenuButton>
-                <IonIcon
-                  color={`dark-${theme}`}
-                  icon={menuOutline}
-                  size="large"
-                />
-                {needUpdate && <Badge />}
-              </IonMenuButton>
-            </IonTabButton>
-
-            <IonTabButton
-              tab="home"
-              href="/tabs/home"
-              className={theme}
-            >
-              <IonLabel>{t("Home")}</IonLabel>
-            </IonTabButton>
-            <IonTabButton
-              tab="details"
-              href="/tabs/details"
-              className={theme}
-            >
-              <IonLabel>{t("Details")}</IonLabel>
-            </IonTabButton>
-          </IonTabBar>
-        </IonToolbar>
       </IonHeader>
       <IonContent id="tabs-content" color={`background-${theme}`}>
         <IonTabs>
@@ -91,12 +55,7 @@ const TabsContainer: React.FC<TabsContainerProps> = ({ theme, needUpdate }) => {
             <IonTabButton
               tab="menu"
               href="#"
-              style={{
-                background: `var(--ion-color-transparent-${theme})`,
-                border: `var(--ion-color-transparent-${theme})`,
-                maxWidth: "30px",
-                marginLeft: "15px",
-              }}
+              className="menu-button"
             >
               <IonMenuButton>
                 <IonIcon
@@ -110,16 +69,14 @@ const TabsContainer: React.FC<TabsContainerProps> = ({ theme, needUpdate }) => {
             <IonTabButton
               tab="home"
               href="/tabs/home"
-              className={theme}
-              style={{ marginLeft: "auto" }}
+              className={`${theme} home-tab`}
             >
               <IonLabel>{t("Home")}</IonLabel>
             </IonTabButton>
             <IonTabButton
               tab="details"
               href="/tabs/details"
-              className={theme}
-              style={{ marginLeft: "15px", marginRight: "20px" }}
+              className={`${theme} details-tab`}
             >
               <IonLabel>{t("Details")}</IonLabel>
             </IonTabButton>
